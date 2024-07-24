c354906e07f98786b3261ddedfa1d054813e5e0b
NouP1 <smolen43ret@gmail.com>
2024-07-24 17:56:44 +0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const TelegramApi = require('node-telegram-bot-api');
 const axios = require('axios');
 const { back, backError } = require('./options');
-const {logUserAction,checkElasticsearchConnection, logBotError, logUserError}= require('./elastic'); 
+const { logUserAction, checkElasticsearchConnection, logBotError, logUserError } = require('./elastic');
+const { sendPixel } = require('./sendPix')
 require('dotenv').config();
 
 const token = process.env.TOKEN;
+const key = process.env.KEY;
 const metaAccessTokenFirst = process.env.LONG_LIVED_TOKEN_FIRST;
 const metaAccessTokenSecond = process.env.LONG_LIVED_TOKEN_SECOND;
 
@@ -17,7 +19,7 @@ const bot = new TelegramApi(token, { polling: true });
 const start = async () => {
     await checkElasticsearchConnection();
     bot.setMyCommands([
-        { command: '/start', description: "Запустить бота заново" },
+        { command: '/start', description: "Перезапуск бота" },
     ]);
 
     const user = {
@@ -26,7 +28,7 @@ const start = async () => {
         accountId: null,
         businessId: null,
     };
-
+    let authorizedUsers = {};
     let inlineKeyboard = [];
 
     bot.on('message', async msg => {
@@ -39,61 +41,38 @@ const start = async () => {
             console.log(`Пользователь ${username},${firstname}\n Отправил сообщение в бот: ${text}`)
 
             if (text === '/start') {
+                if (!authorizedUsers[chatId]) {
+                    await bot.sendMessage(chatId, "Привет, введи ключ!")
 
-                const [listpixelsFirst, listpixelsSecond] = await Promise.all([
-                    axios.get(`https://graph.facebook.com/v20.0/${businessIdFirst}/adspixels?fields=name`, {
-                        headers: {
-                            'Authorization': `Bearer ${metaAccessTokenFirst}`
+                } else {
+
+                    authorizedUsers[chatId] = true;
+                    await sendPixel(chatId);
+
+                    await bot.sendMessage(chatId, "Привет, выбирай нужный тебе пиксель", {
+                        reply_markup: {
+                            inline_keyboard: inlineKeyboard
                         }
-                    }),
-                    axios.get(`https://graph.facebook.com/v20.0/${businessIdSecond}/adspixels?fields=name`, {
-                        headers: {
-                            'Authorization': `Bearer ${metaAccessTokenSecond}`
+                    });
+                }
+
+            } else if (!authorizedUsers[chatId]) {
+                // Проверяем ключ, если пользователь не авторизован
+                if (text === key) {
+                    authorizedUsers[chatId] = true;
+                    await sendPixel(chatId);
+                    await bot.sendMessage(chatId, "Выбирай нужный тебе пиксель", {
+                        reply_markup: {
+                            inline_keyboard: inlineKeyboard
                         }
-                    })
-                ]);
+                    });
+                } else {
+                    await bot.sendMessage(chatId, "Неверный ключ!");
+                }
 
-                const pixelPurposes = {
-                    '1092606521936943': 'Для pwa прил тир 3',
-                    '896644941674454': 'Для iOS ZM',
-                    '786851899966322': 'Для iOS Buzz прил',
-                    '961413918926551': 'Для pwa тир 1-2'
-                };
-            
-                // Берем первые три пикселя из первого бизнес-менеджера и первый пиксель из второго
-                const pixelsFirst = listpixelsFirst.data.data.slice(0, 3).map(pixel => ({
-                    ...pixel,
-                    businessId: businessIdFirst,
-                    name: `${pixel.name} - ${pixelPurposes[pixel.id] || ''}`
-                }));
-                const pixelSecond = listpixelsSecond.data.data.slice(0, 1).map(pixel => ({
-                    ...pixel,
-                    businessId: businessIdSecond,
-                    name: `${pixel.name} - ${pixelPurposes[pixel.id] || ''}`
-                }));
-            
-                // Объединяем массивы
-                const pixels = [
-                    ...pixelsFirst,
-                    ...pixelSecond
-                ];
-
-                // Формируем массив кнопок
-                inlineKeyboard = pixels.map(pixel => [{
-                    text: pixel.name,
-                    callback_data: JSON.stringify({ pixelId: pixel.id, businessId: pixel.businessId })
-                }]);
-
-                await bot.sendMessage(chatId, "Привет, выбирай нужный тебе пиксель", {
-                    reply_markup: {
-                        inline_keyboard: inlineKeyboard
-                    }
-                });
-            }
-
-            else {
+            } else {
                 try {
-                    
+                    await bot.sendMessage(chatId, "Ошибка или успех шеринга");
                     user.accountId = text;
                     const metaAccessToken = user.businessId === businessIdFirst ? metaAccessTokenFirst : metaAccessTokenSecond;
                     const sharedPixels = await axios.post(`https://graph.facebook.com/v20.0/${user.pixelId}/shared_accounts`,
@@ -107,7 +86,7 @@ const start = async () => {
                             }
                         })
 
-                    await bot.sendMessage(chatId,"Пиксель успешно пошерен! 👌", back)
+                    await bot.sendMessage(chatId, "Пиксель успешно пошерен! 👌", back)
 
                     console.log(
                         "Пиксель успешно пошерен!:\n" +
@@ -118,7 +97,7 @@ const start = async () => {
                         "   Статус: " + JSON.stringify(sharedPixels.data, null, 2)
                     )
 
-                    logUserAction(chatId, username,firstname, 'Успешно пошерил пиксель',{
+                    logUserAction(chatId, username, firstname, 'Успешно пошерил пиксель', {
                         pixelId: user.pixelId,
                         pixelName: user.pixelName,
                         accountId: user.accountId,
@@ -134,22 +113,22 @@ const start = async () => {
                 } catch (error) {
                     if (error.response) {
                         console.error('Ошибка от сервера:', error.response.data);
-                        const { error_user_msg, code , message } = error.response.data.error;
+                        const { error_user_msg, code, message } = error.response.data.error;
                         await bot.sendMessage(chatId, "Произошла ошибка при попытке шеринга пикселя.\n" +
-                            "Код ошибки: " + code + "\n" + message+"\n" + error_user_msg, backError);
-                       
-                            logUserError(chatId,username,firstname, 'share_pixel_error',{pixelName: user.pixelName,accountId: user.accountId,code,message,error_user_msg})
-                               
-                            
+                            "Код ошибки: " + code + "\n" + message + "\n" + error_user_msg, backError);
+
+                        logUserError(chatId, username, firstname, 'share_pixel_error', { pixelName: user.pixelName, accountId: user.accountId, code, message, error_user_msg })
+
+
 
                     } else if (error.request) {
-                        const { error_user_msg, code , message } = error.request.data.error;
+                        const { error_user_msg, code, message } = error.request.data.error;
                         console.error('Ошибка запроса:', error.request);
                         console.error(error)
                         await bot.sendMessage(chatId, "Произошла ошибка при попытке шеринга пикселя.");
-                        
-                        logUserError(chatId,username,firstname, 'share_pixel_error',{pixelName: user.pixelName,accountId: user.accountId,code,message,error_user_msg});
-                    
+
+                        logUserError(chatId, username, firstname, 'share_pixel_error', { pixelName: user.pixelName, accountId: user.accountId, code, message, error_user_msg });
+
                     }
 
                 }
@@ -179,7 +158,7 @@ const start = async () => {
             console.log('data:', data);
             console.log('message.message_id:', messageId);
 
-            if (data && data !== 'selectPixel'&& data !== 'edditAccountId') {
+            if (data && data !== 'selectPixel' && data !== 'edditAccountId') {
                 const parsedData = JSON.parse(data);
                 user.pixelId = parsedData.pixelId;
                 user.businessId = parsedData.businessId;
@@ -189,7 +168,7 @@ const start = async () => {
                     .find(button => JSON.parse(button.callback_data).pixelId === user.pixelId);
                 user.pixelName = selectedPixel.text;
 
-                await bot.editMessageText("Отправь мне номер аккаунта, на который нужно пошерить выбранный пиксель", {
+                await bot.editMessageText("Отправь мне номер account_id, на который нужно пошерить выбранный пиксель", {
                     chat_id: chatId,
                     message_id: messageId,
                     reply_markup: back.reply_markup
@@ -216,7 +195,7 @@ const start = async () => {
                     '786851899966322': 'Для iOS Buzz прил',
                     '961413918926551': 'Для pwa тир 1-2'
                 };
-            
+
                 // Берем первые три пикселя из первого бизнес-менеджера и первый пиксель из второго
                 const pixelsFirst = listpixelsFirst.data.data.slice(0, 3).map(pixel => ({
                     ...pixel,
@@ -228,7 +207,7 @@ const start = async () => {
                     businessId: businessIdSecond,
                     name: `${pixel.name} - ${pixelPurposes[pixel.id] || ''}`
                 }));
-            
+
                 // Объединяем массивы
                 const pixels = [
                     ...pixelsFirst,
@@ -251,11 +230,12 @@ const start = async () => {
                 })
             }
             if (data === 'edditAccountId') {
-                await bot.editMessageText('Отправь мне номер аккаунта, на который нужно пошерить выбранный пиксель', {
+                await bot.editMessageText('Отправь мне номер account_id, на который нужно пошерить выбранный пиксель', {
                     chat_id: chatId,
                     message_id: messageId,
-                reply_markup:back.reply_markup});
-            
+                    reply_markup: back.reply_markup
+                });
+
             }
 
         } catch (error) {
---
+++ sendPix.js
@@ -0,0 +1,54 @@
+const metaAccessTokenFirst = process.env.LONG_LIVED_TOKEN_FIRST;
+const metaAccessTokenSecond = process.env.LONG_LIVED_TOKEN_SECOND;
+
+const businessIdFirst = process.env.BUSINESS_ID_FIRST;
+const businessIdSecond = process.env.BUSINESS_ID_SECOND;
+
+async function sendPixel(chatId) {
+    const [listpixelsFirst, listpixelsSecond] = await Promise.all([
+        axios.get(`https://graph.facebook.com/v20.0/${businessIdFirst}/adspixels?fields=name`, {
+            headers: {
+                'Authorization': `Bearer ${metaAccessTokenFirst}`
+            }
+        }),
+        axios.get(`https://graph.facebook.com/v20.0/${businessIdSecond}/adspixels?fields=name`, {
+            headers: {
+                'Authorization': `Bearer ${metaAccessTokenSecond}`
+            }
+        })
+    ]);
+
+    const pixelPurposes = {
+        '1092606521936943': 'Для pwa прил тир 3',
+        '896644941674454': 'Для iOS ZM',
+        '786851899966322': 'Для iOS Buzz прил',
+        '961413918926551': 'Для pwa тир 1-2'
+    };
+
+    // Берем первые три пикселя из первого бизнес-менеджера и первый пиксель из второго
+    const pixelsFirst = listpixelsFirst.data.data.slice(0, 3).map(pixel => ({
+        ...pixel,
+        businessId: businessIdFirst,
+        name: `${pixel.name} - ${pixelPurposes[pixel.id] || ''}`
+    }));
+    const pixelSecond = listpixelsSecond.data.data.slice(0, 1).map(pixel => ({
+        ...pixel,
+        businessId: businessIdSecond,
+        name: `${pixel.name} - ${pixelPurposes[pixel.id] || ''}`
+    }));
+
+    // Объединяем массивы
+    const pixels = [
+        ...pixelsFirst,
+        ...pixelSecond
+    ];
+
+    // Формируем массив кнопок
+    inlineKeyboard = pixels.map(pixel => [{
+        text: pixel.name,
+        callback_data: JSON.stringify({ pixelId: pixel.id, businessId: pixel.businessId })
+    }]);
+
+   
+}
+    module.exports = {sendPixel}
